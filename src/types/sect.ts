597e9083ac4551405a4a459a10ca448848b90643
baileykv75-netizen <baileykv75@gmail.com>
2026-08-18 16:40:48 +0800

    R24: add authoritative sect membership types

---
+++ src/types/sect.ts
@@ -0,0 +1,21 @@
+export type SectId = 'qingyun'
+export type SectRank = 'service' | 'outer' | 'inner' | 'true'
+export type QingyunJoinPath = 'regular-recruitment' | 'clan-recommendation' | 'steward-family' | 'mortal-service'
+
+export interface SectMembershipState {
+  sectId: SectId
+  rank: SectRank
+  joinedDay: number
+  joinPath: QingyunJoinPath
+}
+
+export interface SectAccess {
+  publicArea: true
+  outerRegistry: boolean
+  serviceArea: boolean
+  basicTeaching: boolean
+  discipleCultivationArea: boolean
+  affairsHallEntry: boolean
+  innerResources: boolean
+  trueInheritance: boolean
+}
